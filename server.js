7d4e159d429d9ea9186b93699a07549408b5fc83
heizmann <sacha.heizmann@epfl.ch>
2025-05-25 00:22:58 +0200

    updt port.

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3002;
 
 // Hard‑coded reserved dates for demo
 const events = [
